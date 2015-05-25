9f798c1d2a4d283e4d30a7f566c0dee02c3dd4b1
Auston Salvana <auston.salvana@live.com>
2015-05-25 08:02:39 -0700

    Initial Commit

---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Auston Salvana on 5/25/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
